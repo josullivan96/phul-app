a64c28b65ded1a9aae1b686bb6722b0d1b52c927
Jamie O'Sullivan <josullivan96@hotmail.com>
2022-07-21 22:23:09 -0400

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    NotFoundPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
